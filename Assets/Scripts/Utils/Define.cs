0608fefc7ceda482dbb3207529651209463f26b2
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2024-02-24 22:04:48 +0900

    #5-2 Camera #2

--- Assets/Scripts/Utils/Define.cs
+++ Assets/Scripts/Utils/Define.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Define : MonoBehaviour
+public class Define
 {
+    public enum MouseEvent
+    {
+        Press,
+        Click,
+    }
     public enum CameraMode
     {
         QuarterView,
