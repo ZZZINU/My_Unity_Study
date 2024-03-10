8af002b7ded3a18ad20f733247c73c60d2f8d3cc
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2024-03-10 12:48:16 +0900

    #8-2 Scene Manager #2

--- Assets/Scripts/Scenes/LoginScene.cs
+++ Assets/Scripts/Scenes/LoginScene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoginScene : BaseScene
 {
@@ -12,7 +13,17 @@ public class LoginScene : BaseScene
 
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Managers.Scene.LoadScene(Define.Scene.Game);
+        }
+    }
+
+
     public override void Clear()
     {
+        Debug.Log("LoginScene Clear!");
     }
 }
