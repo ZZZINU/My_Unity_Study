5f15f4e7faf6006f0d560bd2dc9f00fa486f44d2
ZZZINU <nnjin987@gmail.com>
2024-03-24 14:24:24 +0900

    #12-1 Data Manager #2

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-    Coroutine co;
+
     protected override void Init()
     {
         base.Init();
@@ -13,33 +13,11 @@ public class GameScene : BaseScene
 
         Managers.UI.ShowSceneUI<UI_Inven>();
 
-        co = StartCoroutine("ExplodeAfterSeconds", 4.0f);
-
-        StartCoroutine("CoStopExplode", 2.0f);
-    }
-
-    IEnumerator CoStopExplode(float seconds)
-    {
-        Debug.Log("Stop Enter");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Stop Execute!!!");
-        if (co != null)
-        {
-            StopCoroutine(co);
-            co = null;
-        }
-    }
-
-    IEnumerator ExplodeAfterSeconds(float seconds)
-    {
-        Debug.Log("Explode Enter");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Explode Execute!!!");
-        co = null;
+        Dictionary<int, Stat> dict = Managers.Data.StatDict;
     }
 
     public override void Clear()
     {
+        
     }
-
 }
